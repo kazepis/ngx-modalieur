1401f4134b22fcb6fcbd7e3499598518765a3015
Nikolas Kazepis <nick.kazepis@gmail.com>
2026-06-22 17:02:40 +0300

    fix: wrap dialog in .modal so Bootstrap modal CSS variables (background, padding) apply

--- projects/ngx-modalieur/src/lib/bootstrap-dialog-container.ts
+++ projects/ngx-modalieur/src/lib/bootstrap-dialog-container.ts
@@ -14,18 +14,23 @@ import { BOOTSTRAP_MODAL_OPTIONS } from './bootstrap-modal-options';
   selector: 'mdlr-bootstrap-dialog',
   standalone: true,
   imports: [CdkPortalOutlet],
+  // The `.modal` wrapper is required: Bootstrap declares the `--bs-modal-*`
+  // CSS variables (background, padding, width, etc.) on `.modal`, and provides
+  // the full-screen context for `.modal-dialog` centering/sizing.
   template: `
-    <div
-      class="modal-dialog"
-      [class.modal-dialog-centered]="options.centered !== false"
-      [class.modal-dialog-scrollable]="!!options.scrollable"
-      [class.modal-sm]="options.size === 'sm'"
-      [class.modal-lg]="options.size === 'lg'"
-      [class.modal-xl]="options.size === 'xl'"
-      [class.modal-fullscreen]="options.size === 'fullscreen'"
-    >
-      <div class="modal-content">
-        <ng-template cdkPortalOutlet />
+    <div class="modal" tabindex="-1">
+      <div
+        class="modal-dialog"
+        [class.modal-dialog-centered]="options.centered !== false"
+        [class.modal-dialog-scrollable]="!!options.scrollable"
+        [class.modal-sm]="options.size === 'sm'"
+        [class.modal-lg]="options.size === 'lg'"
+        [class.modal-xl]="options.size === 'xl'"
+        [class.modal-fullscreen]="options.size === 'fullscreen'"
+      >
+        <div class="modal-content">
+          <ng-template cdkPortalOutlet />
+        </div>
       </div>
     </div>
   `,
