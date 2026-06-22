5d1a3a9892cc1a6b7d67999c6b7d77350893e4f6
Nikolas Kazepis <nick.kazepis@gmail.com>
2026-06-22 16:33:43 +0300

    test: add specs for ModalieurService result flow and ModalContent helpers

---
+++ projects/ngx-modalieur/src/lib/modal-content.spec.ts
@@ -0,0 +1,64 @@
+import { Component } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+
+import { ModalContent } from './modal-content';
+import { ModalRef } from './modal-ref';
+import { ModalResult } from './modal-result.enum';
+
+class FakeModalRef {
+  calls: Array<{ result: ModalResult; data: unknown }> = [];
+  close(result: ModalResult = ModalResult.Undefined, data?: unknown): void {
+    this.calls.push({ result, data });
+  }
+}
+
+@Component({ standalone: true, template: '' })
+class TestModal extends ModalContent {
+  callYes(): void {
+    this.yes();
+  }
+  callNo(): void {
+    this.no();
+  }
+  callOk(): void {
+    this.ok();
+  }
+  callCancel(): void {
+    this.cancel();
+  }
+  callData(data: unknown): void {
+    this.respondWithData(data);
+  }
+}
+
+describe('ModalContent', () => {
+  let ref: FakeModalRef;
+  let modal: TestModal;
+
+  beforeEach(() => {
+    ref = new FakeModalRef();
+    TestBed.configureTestingModule({
+      providers: [{ provide: ModalRef, useValue: ref }]
+    });
+    modal = TestBed.createComponent(TestModal).componentInstance;
+  });
+
+  it('maps helpers to the matching ModalResult', () => {
+    modal.callYes();
+    modal.callNo();
+    modal.callOk();
+    modal.callCancel();
+
+    expect(ref.calls).toEqual([
+      { result: ModalResult.Yes, data: undefined },
+      { result: ModalResult.No, data: undefined },
+      { result: ModalResult.Ok, data: undefined },
+      { result: ModalResult.Cancel, data: undefined }
+    ]);
+  });
+
+  it('respondWithData closes with Data and the payload', () => {
+    modal.callData({ name: 'Ada' });
+    expect(ref.calls).toEqual([{ result: ModalResult.Data, data: { name: 'Ada' } }]);
+  });
+});
---
+++ projects/ngx-modalieur/src/lib/modalieur.service.spec.ts
@@ -0,0 +1,85 @@
+import { Dialog } from '@angular/cdk/dialog';
+import { Component } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { Subject } from 'rxjs';
+
+import { ModalOutcome } from './modal-outcome';
+import { ModalResult } from './modal-result.enum';
+import { ModalieurService } from './modalieur.service';
+
+@Component({ standalone: true, template: '' })
+class DummyModal {}
+
+class FakeDialogRef {
+  id = 'test-id';
+  closed = new Subject<unknown>();
+  closeArgs: unknown[] = [];
+
+  close(value?: unknown): void {
+    this.closeArgs.push(value);
+    this.closed.next(value);
+    this.closed.complete();
+  }
+}
+
+class FakeDialog {
+  ref = new FakeDialogRef();
+  open(): FakeDialogRef {
+    return this.ref;
+  }
+}
+
+describe('ModalieurService', () => {
+  let service: ModalieurService;
+  let dialog: FakeDialog;
+
+  beforeEach(() => {
+    dialog = new FakeDialog();
+    TestBed.configureTestingModule({
+      providers: [ModalieurService, { provide: Dialog, useValue: dialog }]
+    });
+    service = TestBed.inject(ModalieurService);
+  });
+
+  it('emits the outcome passed to close()', () => {
+    const received: ModalOutcome[] = [];
+    service.show(DummyModal).subscribe(o => received.push(o));
+
+    dialog.ref.close({ result: ModalResult.Yes, data: { ok: true } });
+
+    expect(received).toEqual([{ result: ModalResult.Yes, data: { ok: true } }]);
+  });
+
+  it('falls back to Undefined when the modal is dismissed', () => {
+    const received: ModalOutcome[] = [];
+    service.show(DummyModal).subscribe(o => received.push(o));
+
+    dialog.ref.close(undefined);
+
+    expect(received).toEqual([{ result: ModalResult.Undefined }]);
+  });
+
+  it('showUntilCondition closes only on a truthy emission', () => {
+    const condition = new Subject<boolean>();
+    const received: ModalOutcome[] = [];
+    service.showUntilCondition(DummyModal, condition).subscribe(o => received.push(o));
+
+    condition.next(false);
+    expect(dialog.ref.closeArgs.length).toBe(0);
+
+    condition.next(true);
+    expect(dialog.ref.closeArgs.length).toBe(1);
+    expect(received).toEqual([{ result: ModalResult.Undefined, data: undefined }]);
+  });
+
+  it('showAndReturnRef exposes the id and closes programmatically', () => {
+    const ref = service.showAndReturnRef(DummyModal);
+    const received: ModalOutcome[] = [];
+    ref.closed$.subscribe(o => received.push(o));
+
+    expect(ref.id).toBe('test-id');
+
+    ref.close(ModalResult.Cancel);
+    expect(received).toEqual([{ result: ModalResult.Cancel, data: undefined }]);
+  });
+});
