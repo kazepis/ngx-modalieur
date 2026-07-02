4962e75800bdf61cc663485a4119fb3d65a32711
Nikolas Kazepis <nick.kazepis@gmail.com>
2026-07-02 16:41:42 +0300

    feat: update modal components to improve accessibility with A11yModule and focus management

--- projects/ngx-modalieur-demo-app/src/app/modals/confirm-modal.component.ts
+++ projects/ngx-modalieur-demo-app/src/app/modals/confirm-modal.component.ts
@@ -1,3 +1,4 @@
+import { A11yModule } from '@angular/cdk/a11y';
 import { Component } from '@angular/core';
 import { ModalContent } from 'ngx-modalieur';
 
@@ -5,18 +6,20 @@ import { SampleData } from './sample-data';
 
 @Component({
   selector: 'app-confirm-modal',
-  standalone: true,
+  imports: [A11yModule],
   template: `
-    <div class="modal-header">
-      <h5 class="modal-title">{{ data.title }}</h5>
-      <button type="button" class="btn-close" aria-label="Close" (click)="cancel()"></button>
-    </div>
-    <div class="modal-body">
-      <p class="mb-0 text-break text-pre-line">{{ data.message }}</p>
-    </div>
-    <div class="modal-footer">
-      <button type="button" class="btn btn-secondary" (click)="no()">No</button>
-      <button type="button" class="btn btn-primary" (click)="yes()">Yes</button>
+    <div cdkTrapFocus [cdkTrapFocusAutoCapture]="true">
+      <div class="modal-header">
+        <h5 class="modal-title">{{ data.title }}</h5>
+        <button type="button" class="btn-close" aria-label="Close" (click)="cancel()"></button>
+      </div>
+      <div class="modal-body">
+        <p class="mb-0 text-break text-pre-line">{{ data.message }}</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" (click)="no()">No</button>
+        <button type="button" class="btn btn-primary" (click)="yes()" cdkFocusInitial>Yes</button>
+      </div>
     </div>
   `
 })
--- projects/ngx-modalieur/src/lib/components/message-box/message-box.dialog.ts
+++ projects/ngx-modalieur/src/lib/components/message-box/message-box.dialog.ts
@@ -1,9 +1,10 @@
+import { A11yModule } from '@angular/cdk/a11y';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 import { ModalContent } from '../../modal-content';
 import { ModalResult } from '../../modal-result.enum';
-import { describeMessageBoxButton, MESSAGE_BOX_BUTTON_SETS } from './message-box-buttons.config';
 import { MessageBoxButton } from './message-box-button';
+import { describeMessageBoxButton, MESSAGE_BOX_BUTTON_SETS } from './message-box-buttons.config';
 import { MessageBoxButtons } from './message-box-buttons.enum';
 import { MessageBoxOptions } from './message-box-options';
 
@@ -22,28 +23,43 @@ export const MESSAGE_BOX_BODY_ID = 'mdlr-message-box-body';
  */
 @Component({
   selector: 'mdlr-message-box',
-  standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [A11yModule],
   template: `
-    <div class="modal-header">
-      <ng-content select="[mbHeader]">
-        <h5 class="modal-title" [attr.id]="titleId">{{ title }}</h5>
-        <button type="button" class="btn-close" aria-label="Close" (click)="close(Result.Cancel)"></button>
-      </ng-content>
+    <div cdkTrapFocus [cdkTrapFocusAutoCapture]="true">
+      <div class="modal-header">
+        <ng-content select="[mbHeader]">
+          <h5 class="modal-title" [attr.id]="titleId">{{ title }}</h5>
+          <button type="button" class="btn-close" aria-label="Close" (click)="close(Result.Cancel)"></button>
+        </ng-content>
+      </div>
+      <div class="modal-body" [attr.id]="bodyId">
+        <ng-content select="[mbBody]">{{ message }}</ng-content>
+      </div>
+      <div class="modal-footer">
+        <ng-content select="[mbFooter]">
+          @for (button of buttons; track button.result; let last = $last) {
+            @if (last) {
+              <button type="button" class="btn {{ button.cssClass }}" (click)="close(button.result)" cdkFocusInitial>
+                {{ button.label }}
+              </button>
+            } @else {
+              <button type="button" class="btn {{ button.cssClass }}" (click)="close(button.result)">
+                {{ button.label }}
+              </button>
+            }
+          }
+        </ng-content>
+      </div>
     </div>
-    <div class="modal-body" [attr.id]="bodyId">
-      <ng-content select="[mbBody]">{{ message }}</ng-content>
-    </div>
-    <div class="modal-footer">
-      <ng-content select="[mbFooter]">
-        @for (button of buttons; track button.result) {
-          <button type="button" class="btn {{ button.cssClass }}" (click)="close(button.result)">
-            {{ button.label }}
-          </button>
-        }
-      </ng-content>
-    </div>
-  `
+  `,
+  styles: [
+    `
+      button[cdkFocusInitial]:focus {
+        box-shadow: var(--bs-btn-focus-box-shadow);
+      }
+    `
+  ]
 })
 export class MessageBoxDialog extends ModalContent<MessageBoxOptions | null, never> {
   protected readonly Result = ModalResult;
